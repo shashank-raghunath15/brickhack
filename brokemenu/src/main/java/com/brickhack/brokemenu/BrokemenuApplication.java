56b75b4022c916cd9809fb5dbf9f7d326be94bd8
Shashank Raghunath <shashank.raghunath15@gmail.com>
2018-01-27 16:50:03 -0500

    Init

---
+++ brokemenu/src/main/java/com/brickhack/brokemenu/BrokemenuApplication.java
@@ -0,0 +1,12 @@
+package com.brickhack.brokemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokemenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokemenuApplication.class, args);
+	}
+}
---
+++ brokemenu/src/test/java/com/brickhack/brokemenu/BrokemenuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brickhack.brokemenu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrokemenuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
